95cce23cb2563b5e1b470682cbfbef51ed835cb9
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-13 20:36:38 -0300

    testando requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,19 @@
-const fetchProducts = () => {
+const fetch = require('node-fetch');
+
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
+const fetchProducts = async (produto) => {
   // seu código aqui
+  let results = [];
+  try {
+  const request = await fetch(`${BASE_URL}${produto}`);
+  const response = await request.json();
+  results = await response;
+  return results.results;
+  } catch (error) {
+    console.error(error);
+    throw error.message;
+  }
 };
 
 if (typeof module !== 'undefined') {
@@ -7,3 +21,6 @@ if (typeof module !== 'undefined') {
     fetchProducts,
   };
 }
+
+fetchProducts('computador')
+  .then((result) => console.log(result));
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,25 @@
-require('../mocks/fetchSimulator');
+const { expect } = require('expect');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const fetchSimulator = require('../mocks/fetchSimulator');
 const computadorSearch = require('../mocks/search');
+
+import fetch from 'node-fetch';
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    const actual = fetchProducts;
+    expect(actual).toBeInstanceOf(Function);
+  });
+
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    const received = await fetchProducts('computador');
+    expect(received[0].site_id).toBe('MLB');
+  });
+  
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  await fetchProducts('computador');
+  expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
